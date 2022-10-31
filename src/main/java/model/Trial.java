35e0400dcaceb4487b9056e3acf7ec81ce638c74
Stig <s186333@student.dtu.dk>
2022-10-31 20:00:00 +0100

    added stores

--- src/main/java/model/Trial.java
+++ src/main/java/model/Trial.java
@@ -1,14 +1,28 @@
 package model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "trial")
+@Getter
+@Setter
+@ToString
 public class Trial {
     @Id
     @GeneratedValue
     @Column(name = "id")
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     @Column(name = "name")
     private String name;
--- src/main/java/service/TrialsService.java
+++ src/main/java/service/TrialsService.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import model.Trial;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,9 +13,22 @@ import java.util.List;
 @Path("gettrials")
 public class TrialsService {
     //TODO: replace with real database
-    List<String> trials = Arrays.asList("Melman", "Elmer");
+
+    private String company = "Novo Nordisk";
+    private String trials = "Diabetes";
+    private String location = "Hillerød";
+    private String description = "Diabetes is a disease in which your blood glucose, or blood sugar, levels are too high. Glucose comes from the foods you eat. Insulin is a hormone that helps the glucose get into your cells to give them energy. With diabetes, your body either doesn't make enough insulin or can't use its own insulin as well as it should. This causes too much glucose to stay in your blood. You can also have prediabetes. This means that your blood sugar is higher than normal but not high enough to be called diabetes. Having prediabetes puts you at a higher risk of getting type 2 diabetes. Type 1 diabetes used to be called juvenile diabetes or insulin-dependent diabetes. Type 2 diabetes used to be called adult-onset diabetes. But people of all ages and sizes can get type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most common type of diabetes. It used to be called adult-onset diabetes. But more and more children and teens are getting type 2 diabetes. Type 2 diabetes is the most";
+    //List<String> trials = Arrays.asList("Melman", "Elmer");
+//    @GET
+//    public List<String> getTrials(){
+//        return trials;
+//    }
+    Trial trial = new Trial();
+
+
+
     @GET
-    public List<String> getTrials(){
+    public String getTrials(){
         return trials;
     }
 
