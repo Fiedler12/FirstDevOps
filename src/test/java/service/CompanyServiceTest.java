7ac6708e566784eb464f513aa7e5eda755b617db
Stig <s186333@student.dtu.dk>
2022-11-04 12:46:20 +0100

    added new tests for CompanyService

---
+++ src/test/java/service/CompanyServiceTest.java
@@ -0,0 +1,46 @@
+package service;
+
+import model.Company;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyServiceTest {
+
+    // test the getCompanies method
+    @org.junit.jupiter.api.Test
+    void getCompanies() {
+        // create a new instance of the service
+        CompanyService companyService = new CompanyService();
+        // call the getCompanies method
+        List<Company> companies = companyService.getCompanies();
+        // check that the list is not null
+        assertNotNull(companies);
+        // check that the list is not empty
+        assertFalse(companies.isEmpty());
+        // check that the list contains the expected number of companies
+        assertEquals(5, companies.size());
+        // check that the first company in the list has the expected name
+        assertEquals("BSucker", companies.get(0).getCname());
+    }
+
+    // test the getCompany method with a valid id
+    @org.junit.jupiter.api.Test
+    void getCompany() {
+        // create a new instance of the service
+        CompanyService companyService = new CompanyService();
+        // call the getCompany method with a valid id
+        Company company = companyService.getCompany(1100);
+        // check that the company is not null
+        assertNotNull(company);
+        // call the getCompany method with an invalid id
+        Company company2 = companyService.getCompany(1);
+        // check that the company is null
+        assertNull(company2);
+        // check that the company has the expected name
+        assertEquals("BSucker", company.getCname());
+        // check that the company has the expected email
+        assertEquals("vamp@ink.com", company.getEmail());
+    }
+}
