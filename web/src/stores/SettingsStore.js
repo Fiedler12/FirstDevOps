2e2ad91e542ecc23e196f346e736d375cfa84b35
Stig <s186333@student.dtu.dk>
2022-11-05 12:27:43 +0100

    added Authorization to SettingsStore.js

--- web/src/stores/SettingsStore.js
+++ web/src/stores/SettingsStore.js
@@ -3,7 +3,7 @@ import {tokenstore} from "./TokenStore";
 
 const baseUrl = process.env.NODE_ENV === 'development' ?  "http://localhost:8080/":""; //Check if dev environment
 
-const Loginstates = {LOGGING_IN:"Loading", LOGGEDOUT:"Logout", LOGGED_IN:"LoggedIn"};
+const states = {LOADING:"Loading", DONE:"done", FAILED:"FAILED"};
 
 
 
@@ -16,13 +16,21 @@ export class SettingsStore {
 
     async fetchSettings (){
         const token = tokenstore.token;
+        this.state = states.LOADING;
         const user = JSON.parse(atob(token.split('.')[1])).user
         const userid = JSON.parse(user).id
         //console.log(JSON.parse(user).id);
-        const response = await fetch(baseUrl + 'api/users/' + userid);
+        const response = await fetch(baseUrl + 'api/users/' + userid, {
+            headers: {
+                Authorization: token
+            }
+        });
         const json = await response.json()
+            .then(this.state = states.DONE)
+            .catch(this.state = states.FAILED);
         console.log(json)
         return json
 
     }
 }
+export const settingsStore = new SettingsStore();
