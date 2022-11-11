6217707af93432194cebc9a9f1086573667e8cab
Fiedler12 <o.lyngholm@gmail.com>
2022-11-11 14:58:49 +0100

    Updating our users from frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Settings from './pages/Settings';
 import Trials from './pages/Trials';
 import About from './pages/About';
@@ -12,6 +12,7 @@ import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import CreateTrial from "./pages/CreateTrial";
 import TrialSignup from "./pages/TrialSignup";
+import { tokenstore } from "./stores/TokenStore"
 
 
 function App() {
@@ -23,11 +24,11 @@ function App() {
             <Route path="/about" element={<About/>}/>
             <Route path="/signin" element={<Signin/>}/>
             <Route path="/signup" element={<Signup/>}/>
-            <Route path="/createTrials" element={<CreateTrial/>}/>
+            <Route path="/createTrials" element={tokenstore.token ? <CreateTrial /> : <Navigate replace to="/signin"/> }/>
             <Route path="/homepage" element={<Homepage/>}/>
-            <Route path="/trials" element={<Trials/>}/>
+            <Route path="/trials" element={tokenstore.token ? <Trials/> : <Navigate replace to="/signin"/> }/>
             <Route path="/settings" element={<Settings/>}/>
-            <Route path="/trialsignup/:id" element={<TrialSignup/>}/>
+            <Route path="/trialsignup/:id" element={tokenstore.token ? <TrialSignup/> : <Navigate replace to="/signin"/> }/>
         </Routes>
     </div>
   );
