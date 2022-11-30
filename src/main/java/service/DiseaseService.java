f45fbe9c329c8956d68d45f841d9e9415bae7da3
harzz <harzz3k@gmail.com>
2022-11-30 21:48:06 +0100

    Diseases can now be added

--- src/main/java/service/DiseaseService.java
+++ src/main/java/service/DiseaseService.java
@@ -1,10 +1,8 @@
 package service;
 
 import controller.HibernateController;
-import jakarta.ws.rs.GET;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.NotAuthorizedException;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import model.Disease;
 import org.hibernate.Transaction;
@@ -31,6 +29,32 @@ public class DiseaseService {
             transaction.rollback();
             session.close();
             throw new NotAuthorizedException("Disease not found");
+        }
+    }
+
+    @POST
+    @Path("/init")
+    public void init() {
+        Session session = hibernateController.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        try{;
+            Disease disease = new Disease();
+            disease.setName("Alzheimer");
+            session.persist(disease);
+            Disease disease1 = new Disease();
+            disease1.setName("Parkinson");
+            session.persist(disease1);
+            Disease disease3 = new Disease();
+            disease.setName("ALS");
+            session.persist(disease3);
+            Disease disease4 = new Disease();
+            disease.setName("Hemorrhoids");
+            session.persist(disease4);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            transaction.rollback();
+            session.close();
         }
     }
 
